88200134efd54fa00728664eb29962a833bcafe9
manuelschuppisser <60382887+manuelschuppisser@users.noreply.github.com>
2020-06-18 11:57:26 +0200

    Updates

---
+++ precache-manifest.dcea4544ecc00924b1bf3df17dc45937.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d3262b23a8d8decb1eb5474abe76b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329e922eacfa329fd95e",
+    "url": "/static/css/main.17c2a9eb.chunk.css"
+  },
+  {
+    "revision": "9fa324fbf63b2be5096e",
+    "url": "/static/js/2.f84d3f35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f84d3f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329e922eacfa329fd95e",
+    "url": "/static/js/main.b32c9a3d.chunk.js"
+  },
+  {
+    "revision": "5404f2974ada98425e4d",
+    "url": "/static/js/runtime-main.49b17446.js"
+  },
+  {
+    "revision": "e0748f46858ca316e4d49f4255945ae6",
+    "url": "/static/media/VanGogh1886.e0748f46.jpg"
+  },
+  {
+    "revision": "e3550fc357a0049dc905b4895094ebd4",
+    "url": "/static/media/VanGogh1887.e3550fc3.jpg"
+  },
+  {
+    "revision": "1db24c0c2487fa45051e538cf6f2cc43",
+    "url": "/static/media/VanGogh1888.1db24c0c.jpg"
+  },
+  {
+    "revision": "68b510e08e79d422f34c03494137bb9a",
+    "url": "/static/media/VanGogh1889.68b510e0.jpg"
+  },
+  {
+    "revision": "b9c44b5a0cbd9621e6b912f6d031e5e8",
+    "url": "/static/media/header.b9c44b5a.jpg"
+  },
+  {
+    "revision": "e2556776103fb66f80737996079dc5d4",
+    "url": "/static/media/logo.e2556776.png"
+  }
+]);
